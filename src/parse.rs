71b5fbae1404e0c2d5d4077cb07cbca40b64f62f
Roman Zanko <roman.zanko.vl@gmail.com>
2026-03-14 20:40:14 +0400

    Modularize `All` parser by moving it to `all_parse` module, updating references, and adding corresponding constructors.

--- src/parse.rs
+++ src/parse.rs
@@ -1,3 +1,4 @@
+use crate::parse::all_parse::{All, all2};
 use crate::parse::delimited_parse::{Delimited, delimited};
 use crate::parse::map_parse::{Map, map};
 use crate::parse::preceded_parse::{Preceded, preceded};
@@ -7,6 +8,7 @@ use crate::parse::strip_whitespace_parse::{StripWhitespace, strip_whitespace};
 use crate::parse::tag_parse::{Tag, tag};
 use crate::parse::unquote_parse::{Unquote, unquote};
 
+mod all_parse;
 mod as_is_parse;
 mod delimited_parse;
 mod map_parse;
@@ -56,84 +58,6 @@ fn do_unquote_non_escaped(input: &str) -> Result<(&str, &str), ()> {
     Ok((&input[1 + quote_byteidx..], &input[..quote_byteidx]))
 }
 
-/// Комбинатор, который требует, чтобы все дочерние парсеры отработали,
-/// (аналог `all` из `nom`)
-#[derive(Debug, Clone)]
-struct All<T> {
-    parser: T,
-}
-impl<A0, A1> Parser for All<(A0, A1)>
-where
-    A0: Parser,
-    A1: Parser,
-{
-    type Dest = (A0::Dest, A1::Dest);
-    fn parse<'a>(&self, input: &'a str) -> Result<(&'a str, Self::Dest), ()> {
-        let (remaining, a0) = self.parser.0.parse(input)?;
-        self.parser
-            .1
-            .parse(remaining)
-            .map(|(remaining, a1)| (remaining, (a0, a1)))
-    }
-}
-/// Конструктор [All] для двух парсеров
-/// (в Rust нет чего-то, вроде variadic templates из C++)
-fn all2<A0: Parser, A1: Parser>(a0: A0, a1: A1) -> All<(A0, A1)> {
-    All { parser: (a0, a1) }
-}
-impl<A0, A1, A2> Parser for All<(A0, A1, A2)>
-where
-    A0: Parser,
-    A1: Parser,
-    A2: Parser,
-{
-    type Dest = (A0::Dest, A1::Dest, A2::Dest);
-    fn parse<'a>(&self, input: &'a str) -> Result<(&'a str, Self::Dest), ()> {
-        let (remaining, a0) = self.parser.0.parse(input)?;
-        let (remaining, a1) = self.parser.1.parse(remaining)?;
-        self.parser
-            .2
-            .parse(remaining)
-            .map(|(remaining, a2)| (remaining, (a0, a1, a2)))
-    }
-}
-/// Конструктор [All] для трёх парсеров
-/// (в Rust нет чего-то, вроде variadic templates из C++)
-fn all3<A0: Parser, A1: Parser, A2: Parser>(a0: A0, a1: A1, a2: A2) -> All<(A0, A1, A2)> {
-    All {
-        parser: (a0, a1, a2),
-    }
-}
-impl<A0, A1, A2, A3> Parser for All<(A0, A1, A2, A3)>
-where
-    A0: Parser,
-    A1: Parser,
-    A2: Parser,
-    A3: Parser,
-{
-    type Dest = (A0::Dest, A1::Dest, A2::Dest, A3::Dest);
-    fn parse<'a>(&self, input: &'a str) -> Result<(&'a str, Self::Dest), ()> {
-        let (remaining, a0) = self.parser.0.parse(input)?;
-        let (remaining, a1) = self.parser.1.parse(remaining)?;
-        let (remaining, a2) = self.parser.2.parse(remaining)?;
-        self.parser
-            .3
-            .parse(remaining)
-            .map(|(remaining, a3)| (remaining, (a0, a1, a2, a3)))
-    }
-}
-/// Конструктор [All] для четырёх парсеров
-/// (в Rust нет чего-то, вроде variadic templates из C++)
-fn all4<A0: Parser, A1: Parser, A2: Parser, A3: Parser>(
-    a0: A0,
-    a1: A1,
-    a2: A2,
-    a3: A3,
-) -> All<(A0, A1, A2, A3)> {
-    All {
-        parser: (a0, a1, a2, a3),
-    }
-}
 /// Комбинатор, который вытаскивает значения из пары `"ключ":значение,`.
 /// Для простоты реализации, запятая всегда нужна в конце пары ключ-значение,
 /// простое '"ключ":значение' читаться не будет
