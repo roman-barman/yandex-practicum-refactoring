89a6db24f0d1f0f091296d7b9ff2023ffde6f7a8
Roman Zanko <roman.zanko.vl@gmail.com>
2026-03-14 23:12:02 +0400

    Rename `I32` to `I32Parser`, update references, enforce non-zero values with `NonZeroI32`, and update tests accordingly.

--- src/parse/std_parse/i32_parse.rs
+++ src/parse/std_parse/i32_parse.rs
@@ -1,9 +1,10 @@
 use crate::parse::Parser;
+use std::num::NonZeroI32;
 
-/// Знаковые числа
+/// Signed numbers
 #[derive(Debug)]
-pub struct I32;
-impl Parser for I32 {
+pub struct I32Parser;
+impl Parser for I32Parser {
     type Dest = i32;
     fn parse<'a>(&self, input: &'a str) -> Result<(&'a str, Self::Dest), ()> {
         let end_idx = input
@@ -12,10 +13,7 @@ impl Parser for I32 {
             .find_map(|(idx, c)| (!c.is_ascii_digit()).then_some(idx))
             .unwrap_or(input.len());
         let value = input[..end_idx].parse().map_err(|_| ())?;
-        if value == 0 {
-            return Err(()); // в наших логах нет нулей, ноль в операции - фикция
-        }
-        Ok((&input[end_idx..], value))
+        Ok((&input[end_idx..], NonZeroI32::new(value).ok_or(())?.get()))
     }
 }
 
@@ -25,11 +23,12 @@ mod test {
 
     #[test]
     fn test_i32() {
-        assert_eq!(I32.parse("411".into()), Ok(("".into(), 411)));
-        assert_eq!(I32.parse("411ab".into()), Ok(("ab".into(), 411)));
-        assert_eq!(I32.parse("".into()), Err(()));
-        assert_eq!(I32.parse("-3".into()), Ok(("".into(), -3)));
-        assert_eq!(I32.parse("0x03".into()), Err(()));
-        assert_eq!(I32.parse("-".into()), Err(()));
+        assert_eq!(I32Parser.parse("411".into()), Ok(("".into(), 411)));
+        assert_eq!(I32Parser.parse("411ab".into()), Ok(("ab".into(), 411)));
+        assert_eq!(I32Parser.parse("".into()), Err(()));
+        assert_eq!(I32Parser.parse("-3".into()), Ok(("".into(), -3)));
+        assert_eq!(I32Parser.parse("0x03".into()), Err(()));
+        assert_eq!(I32Parser.parse("-".into()), Err(()));
+        assert_eq!(I32Parser.parse("0".into()), Err(()));
     }
 }
