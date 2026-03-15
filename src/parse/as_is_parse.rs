b5abebdfda984a3bad0531d1cf3c65fa56fca972
Roman Zanko <roman.zanko.vl@gmail.com>
2026-03-15 17:29:22 +0400

    Rename `AsIs` to `AsIsParser`, update references, add `#[allow(dead_code)]`, and include unit tests for string parsing.

--- src/parse/as_is_parse.rs
+++ src/parse/as_is_parse.rs
@@ -1,11 +1,27 @@
 use crate::parse::Parser;
 
-/// Парсер, возвращающий результат как есть
+/// A parser that returns the result as is
 #[derive(Debug, Clone)]
-pub struct AsIs;
-impl Parser for AsIs {
+#[allow(dead_code)]
+pub struct AsIsParser;
+impl Parser for AsIsParser {
     type Dest = String;
     fn parse<'a>(&self, input: &'a str) -> Result<(&'a str, Self::Dest), ()> {
         Ok((&input[input.len()..], input.into()))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_regular_string() {
+        assert_eq!(AsIsParser.parse("hello"), Ok(("", "hello".to_owned())));
+    }
+
+    #[test]
+    fn parse_empty_string() {
+        assert_eq!(AsIsParser.parse(""), Ok(("", "".to_owned())));
     }
 }
